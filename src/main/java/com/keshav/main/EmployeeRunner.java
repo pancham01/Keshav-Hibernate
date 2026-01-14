dcfad4db6ed660cae5e3865204c8fbb7a426ee9d
pancham01 <108358732+pancham01@users.noreply.github.com>
2026-01-14 15:14:04 +0530

    update

--- src/main/java/com/keshav/main/EmployeeRunner.java
+++ src/main/java/com/keshav/main/EmployeeRunner.java
@@ -12,29 +12,32 @@ import com.keshav.entity.Employee;
 
 public class EmployeeRunner {
 
-	public static void main(String[] args)   {
-		
-		
-		Employee emp1 = new Employee("Avya", "FeMale", 64548,"IND");
-		Employee emp2 = new Employee("Nitin", "FeMale", 64548,"IND");
-		Employee emp3 = new Employee("Neelam", "FeMale", 64548,"IND");
-		Employee emp4 = new Employee("Jack", "Male", 64548,"IND");
-		Employee emp5 = new Employee("Atharv", "Male", 64548,"IND");
-		Employee emp6 = new Employee("Naitik", "Male", 64548,"IND");
-		
+	public static void delById(int id, Session session) {
+//		HQL delete query
+		MutationQuery query = session.createMutationQuery("delete from empp where id = :id");
+		query.setParameter("id", id);
+		query.executeUpdate();
+	}
 
-		
-		Session session =  EmpConfiguration.getSessionFactory().openSession();
+	public static void main(String[] args) {
+
+		Employee emp1 = new Employee("Avya", "FeMale", 64548, "IND");
+		Employee emp2 = new Employee("Nitin", "FeMale", 64548, "IND");
+		Employee emp3 = new Employee("Neelam", "FeMale", 64548, "IND");
+		Employee emp4 = new Employee("Jack", "Male", 64548, "IND");
+		Employee emp5 = new Employee("Atharv", "Male", 64548, "IND");
+		Employee emp6 = new Employee("Naitik", "Male", 64548, "IND");
+
+		Session session = EmpConfiguration.getSessionFactory().openSession();
 		Transaction tx = session.beginTransaction();
-		
+
 //		session.persist(emp1);
 //		session.persist(emp2);
 //		session.persist(emp3);
 //		session.persist(emp4);
 //		session.persist(emp5);
 //		session.persist(emp6);
-		
-		
+
 //		Example of HQL to get all the records
 //		Query query1=session.createQuery("from empp",Employee.class);
 //		List list=query1.list();  
@@ -44,15 +47,11 @@ public class EmployeeRunner {
 //		query1.setFirstResult(2);
 //		query1.setMaxResults(5);
 //		System.out.println(query1.list());
-		
-		
-		
-		
+
 //		Query q=session.createQuery("select max(name) from empp");  
 //		List list1=q.list();  
 //		System.out.println(list1);  
-		
-		
+
 //		HQL update query Using Named Parameters
 //		MutationQuery mutationQuery = session.createMutationQuery("update empp set name=:n where id=:i");  
 //		mutationQuery.setParameter("n","Pankaj");  
@@ -60,12 +59,12 @@ public class EmployeeRunner {
 //		System.out.println("status: "+mutationQuery.executeUpdate());  
 
 //		HQL delete query
-		MutationQuery query=session.createMutationQuery("delete from empp where id = 17");  
-		query.executeUpdate();  
-		
-		Query query1=session.createQuery("from empp",Employee.class);
-		List list=query1.list();  
-		System.out.println(list);
+//		MutationQuery query=session.createMutationQuery("delete from empp where id = 17");  
+//		query.executeUpdate();  
+//		
+//		Query query1=session.createQuery("from empp",Employee.class);
+//		List list=query1.list();  
+//		System.out.println(list);
 
 		tx.commit();
 		session.close();
